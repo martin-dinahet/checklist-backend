ffc3bc35881651f0a9af8f160f8a1e6f4cd6a930
Martin <martin.dinahet@proton.me>
2024-12-07 17:10:52 +0100

    added checklists and tasks modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ChecklistsModule } from './checklists/checklists.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "db/sqlite.db",
+    }),
+    ChecklistsModule,
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
