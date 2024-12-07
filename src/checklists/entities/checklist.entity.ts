8089158654424e421097467389dce02c2bd4be21
Martin <martin.dinahet@proton.me>
2024-12-07 17:22:53 +0100

    added entities spec

--- src/checklists/entities/checklist.entity.ts
+++ src/checklists/entities/checklist.entity.ts
@@ -1,1 +1,33 @@
-export class Checklist {}
+import { Entity } from "typeorm";
+import { PrimaryGeneratedColumn } from "typeorm";
+import { Column } from "typeorm";
+import { OneToMany } from "typeorm";
+import { Task } from "../../tasks/entities/task.entity";
+
+export enum ChecklistStatus {
+  NOT_STARTED = "not started",
+  IN_PROGRESS = "in progress",
+  DONE = "done",
+}
+
+@Entity()
+export class Checklist {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({
+    type: "simple-enum",
+    enum: ChecklistStatus,
+    default: ChecklistStatus.NOT_STARTED,
+  })
+  status: ChecklistStatus;
+
+  @OneToMany(() => Task, (task) => task.checklist)
+  tasks: Array<Task>;
+}
--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,1 +1,20 @@
-export class Task {}
+import { Entity } from "typeorm";
+import { PrimaryGeneratedColumn } from "typeorm";
+import { Column } from "typeorm";
+import { ManyToOne } from "typeorm";
+import { Checklist } from "src/checklists/entities/checklist.entity";
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ default: false })
+  isChecked: boolean;
+
+  @ManyToOne(() => Checklist, (checklist) => checklist.tasks)
+  checklist: Checklist;
+}
