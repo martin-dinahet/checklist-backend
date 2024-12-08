ee635e0d7c86cdf2a5e7d9f9827842cfa69b123a
Martin <martin.dinahet@proton.me>
2024-12-08 16:55:10 +0100

    fixed a bug with typeorm entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { ChecklistsModule } from './checklists/checklists.module';
-import { TasksModule } from './tasks/tasks.module';
+import { ChecklistsModule } from "./checklists/checklists.module";
+import { TasksModule } from "./tasks/tasks.module";
+import { Checklist } from "./checklists/entities/checklist.entity";
+import { Task } from "./tasks/entities/task.entity";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: "sqlite",
       database: "db/sqlite.db",
+      entities: [Checklist, Task],
+      synchronize: true,
     }),
     ChecklistsModule,
     TasksModule,
